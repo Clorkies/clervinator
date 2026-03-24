2c32939447eaa2afe8d77046e411a0721c244d01
Clorkies <clarkisnoob434@gmail.com>
2026-03-24 23:13:14 +0800

    Implemented TokenPosition. Immutable. This represents where a token starts in the source code.

--- src/main/java/com/citu/lexor_interpreter/lexer/token/TokenPosition.java
+++ src/main/java/com/citu/lexor_interpreter/lexer/token/TokenPosition.java
@@ -1,5 +1,43 @@
 package com.citu.lexor_interpreter.lexer.token;
 
-public class TokenPosition {
+import com.citu.lexor_interpreter.lexer.LexerException;
+
+public record TokenPosition(int line, int col) {
+
+    // line & col is 1-based index
+    // int index is 0-based index, to be implemented in the future..
+
+    public TokenPosition {
+        if (line < 1 || col < 1) {
+            throw new LexerException("Line and column must be greater than 0");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return line + ":" + col;
+    }
     
 }
+
+/*
+
+use index for more precise, char by char positioning, in the future..
+
+public record TokenPosition(int line, int col, int index) {
+
+    public TokenPosition {
+        if (line < 1 || col < 1 || index < 0) {
+            throw new LexerException("Line and column must be greater than 0, and index must be greater than or equal to 0");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return line + ":" + col + ":" + index;
+    }
+
+}
+
+
+*/
