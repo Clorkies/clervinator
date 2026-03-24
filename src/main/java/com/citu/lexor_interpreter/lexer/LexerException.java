cf9398f4030bf412d90ffc8b7b26eeb9b7dd3655
Clorkies <clarkisnoob434@gmail.com>
2026-03-24 23:46:17 +0800

    Refactor LexerException to remove unnecessary fields, simplifying the constructor while maintaining error reporting capabilities.

--- src/main/java/com/citu/lexor_interpreter/lexer/LexerException.java
+++ src/main/java/com/citu/lexor_interpreter/lexer/LexerException.java
@@ -3,14 +3,9 @@ package com.citu.lexor_interpreter.lexer;
 import com.citu.lexor_interpreter.lexer.token.TokenPosition;
 
 public class LexerException extends RuntimeException {
-    private final TokenPosition position;
-    private String offendingLexeme;
-
 
     public LexerException(String message, TokenPosition position, String offendingLexeme) {
         super("Lexer error at " + position + ": " + message + " (offending lexeme: '" + offendingLexeme + "')");
-        this.position = position;
-        this.offendingLexeme = offendingLexeme;
     }
 
 }
