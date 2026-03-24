01d8853875ec21edce8105bf63ce861b9299b07e
Clorkies <clarkisnoob434@gmail.com>
2026-03-24 23:30:09 +0800

    Implemented basic skeleton for Lexer custom exceptions.

--- src/main/java/com/citu/lexor_interpreter/lexer/LexerException.java
+++ src/main/java/com/citu/lexor_interpreter/lexer/LexerException.java
@@ -1,5 +1,9 @@
 package com.citu.lexor_interpreter.lexer;
 
-public class LexerException {
+public class LexerException extends RuntimeException {
+
+    public LexerException(String message) {
+        super(message);
+    }
     
 }
